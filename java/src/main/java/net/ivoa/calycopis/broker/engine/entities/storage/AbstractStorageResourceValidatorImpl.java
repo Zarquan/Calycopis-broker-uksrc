48c8cfde359911cc6fe77c142d461bdabee94cf8
Dave Morris <dave.morris@manchester.ac.uk>
2026-06-22 11:05:16 +0100

    Renamed AbstractValidator AbstractValidatorImpl

--- java/src/main/java/net/ivoa/calycopis/broker/engine/entities/storage/AbstractStorageResourceValidatorImpl.java
+++ java/src/main/java/net/ivoa/calycopis/broker/engine/entities/storage/AbstractStorageResourceValidatorImpl.java
@@ -24,14 +24,14 @@
 package net.ivoa.calycopis.broker.engine.entities.storage;
 
 import net.ivoa.calycopis.broker.engine.entities.offerset.OfferSetRequestParserContext;
-import net.ivoa.calycopis.broker.engine.functional.validator.AbstractValidator;
+import net.ivoa.calycopis.broker.engine.functional.validator.AbstractValidatorImpl;
 import net.ivoa.calycopis.schema.spring.model.IvoaAbstractStorageResource;
 
 /**
  * 
  */
 public abstract class AbstractStorageResourceValidatorImpl
-extends AbstractValidator<IvoaAbstractStorageResource, AbstractStorageResourceEntity>
+extends AbstractValidatorImpl<IvoaAbstractStorageResource, AbstractStorageResourceEntity>
 implements AbstractStorageResourceValidator
     {
     }
